4a4f6012befaf8d5e78967d8ca48996556bf3098
hanna <hanna-881@hotmail.com>
2023-03-19 08:42:30 -0400

    updated and added loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,24 @@
 
 let pokemonList = [
 
-    { name: 'Bulbasaur', height: 7, types:
-    ['poison', 'grass']},
+    {
+        name: 'Bulbasaur', height: 7, types:
+            ['poison', 'grass']
+    },
 
-    { name: 'Wigglytuff', height: 10, types:
-    ['fairy', 'normal']},
+    {
+        name: 'Wigglytuff', height: 10, types:
+            ['fairy', 'normal']
+    },
 
-    { name: 'Eevee', height: 3, types:
-    ['normal']},
+    {
+        name: 'Eevee', height: 3, types:
+            ['normal']
+    },
 ];
 
+for (let i = i; i < pokemonList; length; i++) {
+    if (pokemonList[i].height > 7); { 
+        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow! That's big!`);
+    }
+}
