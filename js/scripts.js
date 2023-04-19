b25cba3f6525cb377ed6f38a521119b24b33528c
hanna <hanna-881@hotmail.com>
2023-04-19 18:05:20 -0400

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -19,15 +19,12 @@ let pokemonList = [
 ];
 
 
-pokemonList.push(pokemon1);
-pokemonList.push(pokemon2);
-pokemonList.push(pokemon3);
+let pokemonSize = "Wow that's a large pokemon.";
+
 
 for (let i = 0; i < pokemonList.length; i++) {
-    // check if the height is greater than or equal to 7
-    if (pokemonList[i].height >= 7) {
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!<br>");
-    } else {
-        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
+    if (pokemonList[i].height > 7) {
+        document.write(${ pokemonList[i].name }
+            (height(${ pokemonList[i].height }) - ${ pokemonSize});
     }
 }
