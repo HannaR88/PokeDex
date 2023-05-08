54ee5b6cf113b88d683867fe8ba443a3940cc749
hanna <hanna-881@hotmail.com>
2023-05-08 19:24:25 -0400

    Edit js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // array of objects
 
+
 let pokemonList = [
 
     {
@@ -19,13 +20,25 @@ let pokemonList = [
 ];
 
 
-for (let i = 0; i < pokemonList.length; i++) {
-    const pokemon = pokemonList[i]
-    if (pokemon.height > 7) {
-        document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow that is a large pokemon! <br>" );
-    
-    } else {
-        document.write(pokemon.name + " (height: " + pokemon.height + ")<br>");
+let pokemonRepository = (function () {
+    let pokemonList = [];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-} 
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+
+
+pokemonList.forEach(function (pokemon) {
+    console.log(pokemon.name + ' height ' + pokemon.height);
+});
+
 
