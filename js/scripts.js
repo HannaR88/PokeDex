fea8214aab31c43123e7e5d6bf99dc0b6df42d30
hanna <hanna-881@hotmail.com>
2023-05-09 18:52:28 -0400

    updates scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -20,25 +20,34 @@ let pokemonList = [
 ];
 
 
-let pokemonRepository = (function () {
-    let pokemonList = [];
+//for (let i = 0; i < pokemonList.length; i++) {
+  //  const pokemon = pokemonList[i]
+    //if (pokemon.height > 7) {
+    //    document.write(pokemon.name + " (height: " + pokemon.height + ") - Wow that is a large pokemon! <br>" );
+    
+   // } else {
+   //     document.write(pokemon.name + " (height: " + pokemon.height + ")<br>");
+    //}
 
-    function add(pokemon) {
-        pokemonList.push(pokemon);
-    }
-    function getAll() {
-        return pokemonList;
-    }
-    return {
-        add: add,
-        getAll: getAll
-    };
-})();
+    let pokemonRepository = (function () {
+        let pokemonList = [];
+
+        function add(pokemon) {
+            pokemonList.push(pokemon); 
+           }
+        function getAll() {
+            return pokemonList; 
+        }
+        return {
+            add: add, 
+            getAll: getAll
+        };
+    })();
 
 
 
-pokemonList.forEach(function (pokemon) {
-    console.log(pokemon.name + ' height ' + pokemon.height);
+    pokemonList.forEach(function(pokemon) {
+console.log(pokemon.name + ' height ' + pokemon.height);
 });
 
 
