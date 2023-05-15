f6f8ee664aefc2be2564eb35dd242ec3834669bc
hanna <hanna-881@hotmail.com>
2023-05-15 18:14:30 -0400

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -54,12 +54,11 @@ let pokemonRepository = (function () { //this line along with line 68 is the IIF
         pokemonList.push(pokemon);
     }
 
-    function getAll() {   // this function is what is "getting all" of the pokemon from your pokemonList array
+    function getAll() {   // this function is what is "getting all" of the pokemon from my pokemonList array
 
         return pokemonList;
     }
-    return {   // the IIFE prevents outside functions from altering what is inside of it, to use the the data 
-        //outside of the IIFE these are what are used outside of the IIFE
+    return {   // the IIFE prevents outside functions from altering what is inside of it, to use the the data of the IIFE these are what are used outside of the IIFE
 
         add: add,
         getAll: getAll
@@ -67,8 +66,6 @@ let pokemonRepository = (function () { //this line along with line 68 is the IIF
 })();
 
 
-pokemonRepository.getAll().forEach(function (pokemon) {// here we are calling on 
-    //pokemonRepository the function getAll and on that were using the forEach function with the value pokemon
-    console.log(pokemon.name + ' height ' + pokemon.height); // here we are printing to 
-    //the console the results of the getAll().forEach functions
+pokemonRepository.getAll().forEach(function (pokemon) {// here we are calling on pokemonRepository the function getAll and on that were using the forEach function with the value pokemon
+    console.log(pokemon.name + ' height ' + pokemon.height); // here we are printing to the console the results of the getAll().forEach functions
 });
